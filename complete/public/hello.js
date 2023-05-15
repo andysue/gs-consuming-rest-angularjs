546ad5c4c666b6048e062f41dc0f3e3ece2411cd
andysue <andysue@gmail.com>
2023-05-15 11:51:53 +0800

    test angular

--- complete/public/hello.js
+++ complete/public/hello.js
@@ -1,6 +1,13 @@
 angular.module('demo', [])
 .controller('Hello', function($scope, $http) {
-    $http.get('http://rest-service.guides.spring.io/greeting').
+    $http.get('http://localhost:8081/greeting-javaconfig').
+        then(function(response) {
+            $scope.greeting = response.data;
+        });
+});
+angular.module('demo', [])
+.controller('Test', function($scope, $http) {
+    $http.get('http://localhost:8081/greeting-javaconfig').
         then(function(response) {
             $scope.greeting = response.data;
         });
